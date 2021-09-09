e5a3a61f903a8ffd34088d543288fac840867ad4
shibafu <civaf-th123@hotmail.co.jp>
2021-09-09 23:29:36 +0900

    他ページ遷移

--- pages/other.js
+++ pages/other.js
@@ -4,8 +4,8 @@ import { useState } from 'react'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  const [title, setTitle] = useState("Index")
+export default function Other() {
+  const [title, setTitle] = useState("Other")
   const [message, setMessage] = useState("React Next.js sample page")
 
   return (
@@ -14,15 +14,16 @@ export default function Home() {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous"></link>
       </Head>
+
       <h1 className="bg-primary my-3 text-white display-4">React</h1>
         <div className="container">
           <h3 className="my-3 text-primary text-cener">{title}</h3>
           <div className="container">
             <p>
-              コレはページ移動のサンプルです。
+              コレはもう一つのページの表示です
             </p>
-            <Link href="/other">
-              <a>Go to Other page $gt;#gt;</a>
+            <Link href="/">
+              <a>Back to Indx page $gt;#gt;</a>
             </Link>
           </div>
         </div>
