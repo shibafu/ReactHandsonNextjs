91fb264f0b9ef03fc71ea4ec741b3fc63879c408
shibafu <civaf-th123@hotmail.co.jp>
2021-09-10 20:34:57 +0900

    レイアウト訳

---
+++ pages/component/footer.js
@@ -0,0 +1,10 @@
+export default function Footer(props){
+    return (
+        <div>
+            <h1 className="text-center h6 my-4">
+                {props.footer}
+            </h1>
+        </div>
+    )
+
+}
