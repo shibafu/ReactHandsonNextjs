5c425b2c6361cdffe4fc28dbf002fe8d9837392b
shibafu <civaf-th123@hotmail.co.jp>
2021-09-18 18:59:44 +0900

    API コミット

--- pages/index.js
+++ pages/index.js
@@ -6,29 +6,40 @@ import Layout from './component/layout'
 import Image from './component/image'
 
 export default function Home() {
+ // const url = './public/data.json'
+  const[data, setData] = useState({message:'', data:[]})
+
+  
+  const url = './data.json'
+
+  fetch(url)
+    .then(res => res.json())
+    .then(res => setData(res))
+
   const [title, setTitle] = useState("Index")
   const [message, setMessage] = useState("React Next.js sample page")
 
   return (
     <div>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous"></link>
       <Layout header="Next.js" title="Top Pages" />
-      <Head title={title}>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous"></link>
-      </Head>
-      <h1 className="bg-primary my-3 text-white display-4">React</h1>
-        <div className="container">
-          <h3 className="my-3 text-primary text-cener">{title}</h3>
-          <div className="container">
-            <p>
-              コレはページ移動のサンプルです。
-            </p>
-            <Link href="/other">
-              <a>Go to Other page $gt;#gt;</a>
-            </Link>
-            
-            <Image fname="koishi.jpg" size="400" />
-          </div>
+        <div className="alert alert-primary text-center">
+          <h3 className="mb-4">{data.message}</h3>
+            <table className="title bg-white">
+              <thead className="table-dark">
+                <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+              </thead>
+              <tbody>
+                {data.data.map((value, key) => {
+                  <tr ket={key}>
+                    <th>{value.name}</th>
+                    <th>{value.mail}</th>
+                    <th>{value.age}</th>
+                  </tr>
+                })}
+              </tbody>
+            </table>
         </div>
       </div>
   )
